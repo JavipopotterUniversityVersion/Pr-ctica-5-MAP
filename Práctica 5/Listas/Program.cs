696ae4c399d827c8f0a0c8f29fedffe85e060baa
Stardust_Asher <149375124+StardustAsher@users.noreply.github.com>
2024-04-27 10:12:00 +0200

    a

---
+++ Práctica 5/Listas/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Listas;
+
+namespace Main
+{
+	class MainClass
+	{
+		static string [] ops = {"Quit", "InsertaPpio", "BuscaElto", "nEsimo"};
+		public static void Main(string[] args)
+		{
+
+		}
+		
+
+		
+        
+
+	    
+	}
+
+}
